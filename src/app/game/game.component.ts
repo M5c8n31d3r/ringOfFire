f9ba7234d389e63942af9c3904754a6cf9eb161f
M5c8n31d3r <m.schneider-kleintransporte@web.de>
2023-06-17 14:34:18 +0200

    firebase init

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,33 +1,70 @@
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { Firestore, collection, collectionData, doc, docData, getFirestore, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
-  
+  styleUrls: ['./game.component.scss'],
 })
 
 
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
+  games$: Observable<any[]>;
   game: Game = new Game();
+  gameId: string;
+  games: Array<any>;
 
 
-  constructor(public dialog: MatDialog) { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: Firestore,
+    public dialog: MatDialog
+  ) {
+    const itemCollection = collection(this.firestore, 'games');
+    this.games$ = collectionData(itemCollection);
+  }
 
 
   ngOnInit(): void {
     this.newGame();
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
+      this.gameId = params['id'];
+
+      this.loadGame(this.gameId);
+    });
   }
 
 
   newGame() {
   this.game = new Game();
   console.log(this.game);
+  }
+
+
+  loadGame(gameId) {
+    const gameRef: any = doc(this.firestore, 'games', gameId);
+    this.games$ = docData(gameRef);
+    this.games$.subscribe((game: any) => {
+      console.log('my game', game);
+      this.game.currentPlayer = game.currentPlayer;
+      this.game.playedCards = game.playedCards;
+      this.game.players = game.players;
+      this.game.stack = game.stack;
+      this.game.pickCardAnimation = game.pickCardAnimation;
+      this.game.currentCard = game.currentCard;
+    });
   }
 
 
@@ -64,4 +101,13 @@ export class GameComponent implements OnInit {
     });
   }
 
+
+  saveGame() {
+    const db = getFirestore();
+    const docRef = doc(db, 'games', this.gameId);
+    updateDoc(docRef, this.game.toJson()).then((res) => {
+      console.log('Speichern erfolgreich! ', res);
+    });
+  }
 }
+
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,22 +1,26 @@
+
+
+
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { addDoc, collection } from '@firebase/firestore';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
-  styleUrls: ['./start-screen.component.scss']
+  styleUrls: ['./start-screen.component.scss'],
 })
-
-export class StartScreenComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
-  ngOnInit(): void { 
-  }
-
+export class StartScreenComponent {
+  constructor(private firestore: Firestore, private router: Router) {}
 
   newGame() {
-    //Start Game
-    this.router.navigateByUrl('/game');
+    // Start game
+    let game = new Game();
+    let coll = collection(this.firestore, 'games');
+    addDoc(coll, game.toJson()).then((gameInfo: any) => {
+      this.router.navigateByUrl('/game/' + gameInfo.id);
+    });
   }
 }
